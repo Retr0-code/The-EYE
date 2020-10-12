7b274004b9b30335d3d004f3dcab78f65495b94b
Retr0-code <60541996+Retr0-code@users.noreply.github.com>
2020-10-12 17:33:29 +0300

    Add files via upload

---
+++ TheEYE.py
@@ -0,0 +1,66 @@
+import requests
+import re
+import subprocess
+import sys
+import colorama
+from colorama import Fore
+
+colorama.init()
+
+print("""
+
+by >>>Retr0
+                                
+           _----------_
+        __/     /\     \__
+      _/       /  \       \_
+     /        / ## \        \     
+     \        \ ## /        /
+       \__     \  /     __/ 
+          \_    \/    _/
+            ----------
+""")
+
+
+
+
+pattern = r'[{/}/"/]'
+#pattern2 = r"[</>/(/)/,/=/ /'/][йцукенгшщзхъфывапролджэячсмитьбю]"
+
+
+def location(ip):
+    geo = requests.get(f"http://ip-api.com/json/{ip}")
+
+    firstFormat = re.sub(pattern, "", geo.text)
+
+    secondFormat = re.sub(',', "\n", firstFormat)
+
+    print("\n\n", secondFormat)
+
+
+try:
+    argument = sys.argv[1]
+    address = sys.argv[2]
+
+    if argument == "-u":
+        ping = str(subprocess.Popen(["nslookup", "www.google.com"], stdout = subprocess.PIPE).stdout.read())
+        ipmatch = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", ping)
+        print("\n\n ",Fore.GREEN,"[+]", ipmatch[1])
+        location(ipmatch[1])
+
+    elif argument == "-i":
+        location(address)
+
+except IndexError:
+    print("""
+Usage:
+    
+    -u     uses for url address      TheEYE.py -u www.example.com
+    -i     uses for ipv4 address     TheEYE.py -i 0.0.0.0
+    ______________________________________________________________
+
+    lat is latitude |
+    lon is longitude -
+
+    Made by >>>Retr0
+    """)
